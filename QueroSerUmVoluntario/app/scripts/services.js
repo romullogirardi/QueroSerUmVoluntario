b0e53426dad7c85838be966dec6a548392c5a26f
girardi <girardi.fmce@imbel.gov.br>
2017-07-24 15:11:05 -0300

    Criação do projeto.

---
+++ QueroSerUmVoluntario/app/scripts/services.js
@@ -0,0 +1,36 @@
+'use strict';
+angular.module('volunteerApp')
+
+.constant("baseURL","http://localhost:3000/")
+
+.service('menuFactory', ['$resource', 'baseURL', function($resource,baseURL) {
+
+	this.getDishes = function(){
+        return $resource(baseURL+"dishes/:id", null, {'update':{method:'PUT' }});
+    };
+
+    this.getPromotions = function(){
+        return $resource(baseURL+"promotions/:id", null, {'update':{method:'PUT' }});
+    };
+}])
+
+.service('organizationFactory', ['$resource', 'baseURL', function($resource,baseURL) {
+
+	this.getOrganizations = function(){
+        return $resource(baseURL+"organizations/:id", null, {'update':{method:'PUT' }});
+    };
+}])
+
+.service('corporateFactory', ['$resource', 'baseURL', function($resource,baseURL) {
+
+	this.getLeaderships = function(){
+        return $resource(baseURL+"leadership/:id", null, {'update':{method:'PUT' }});
+    };
+}])
+
+.service('feedbackFactory', ['$resource', 'baseURL', function($resource,baseURL) {
+
+	this.getFeedbacks = function(){
+        return $resource(baseURL+"feedback/:id", null, {'update':{method:'PUT' }});
+    };
+}]);
